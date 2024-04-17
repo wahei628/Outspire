7e42f8c114092e4b2acc7d5e2176c3185ccb866c
wahei628 <wahei628@gmail.com>
2024-04-17 17:01:44 +0900

    DiagnosisResultsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_071638) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_071911) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_071638) do
     t.string "question_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "diagnosis_results", force: :cascade do |t|
+    t.bigint "diagnosis_question_id"
+    t.string "result_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["diagnosis_question_id"], name: "index_diagnosis_results_on_diagnosis_question_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +38,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_071638) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "diagnosis_results", "diagnosis_questions"
 end
