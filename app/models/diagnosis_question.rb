75aef2c4397b02dc975fe99dcde3d89ac124119c
wahei628 <wahei628@gmail.com>
2024-04-23 21:50:45 +0900

    モデル関連付け

--- app/models/diagnosis_question.rb
+++ app/models/diagnosis_question.rb
@@ -1,3 +1,6 @@
 class DiagnosisQuestion < ApplicationRecord
-  
+  # 各質問には複数の診断結果が関連付けられる
+  has_many :diagnosis_results, dependent: :destroy
+  # 各質問には複数のユーザー回答が関連付けられる
+  has_many :user_answers, dependent: :destroy
 end
--- app/models/diagnosis_result.rb
+++ app/models/diagnosis_result.rb
@@ -1,3 +1,4 @@
 class DiagnosisResult < ApplicationRecord
-  
+  # 診断結果は特定の診断質問に属する
+  belongs_to :diagnosis_question
 end
