c4dac1029d2e6e8d711b306b181fa58ac9e46e36
wahei628 <wahei628@gmail.com>
2024-06-03 00:56:52 +0900

    oauthsコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'oauths/oauth'
+  get 'oauths/callback'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   root "static_pages#top"
