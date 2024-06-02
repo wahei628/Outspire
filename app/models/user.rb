15a51963f0ab926aeb59ee6ba2072973b752c69e
wahei628 <wahei628@gmail.com>
2024-06-02 23:50:38 +0900

    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
   # ユーザーが提出したすべての回答を関連付ける
   has_many :user_answers, dependent: :destroy
+  has_many :authentications, dependent: :destroy
+
+  accepts_nested_attributes_for :authentications
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
