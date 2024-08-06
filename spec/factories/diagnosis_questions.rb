22834daa0e52dbe3c43d1d78f2ae5e46c5820083
wahei628 <wahei628@gmail.com>
2024-08-06 13:26:43 +0900

    Rubocopによる自動修正

--- spec/factories/diagnosis_questions.rb
+++ spec/factories/diagnosis_questions.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :diagnosis_question do
-    question_text { "Example question text" }
-    points { { "result1" => 3, "result2" => 2, "result3" => 1 } }
+    question_text { 'Example question text' }
+    points { { 'result1' => 3, 'result2' => 2, 'result3' => 1 } }
   end
 end
--- spec/factories/diagnosis_results.rb
+++ spec/factories/diagnosis_results.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :diagnosis_result do
-    result_text { "Example result text" }
-    key { "result1" }
+    result_text { 'Example result text' }
+    key { 'result1' }
   end
 end
